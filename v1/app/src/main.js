f71fe5187b09a79d4aa963dd94762b982241fb58
sugimrua@DAIV <hiroshi.sugimura@gmail.com>
2023-08-23 21:31:18 +0900

    1.0.1

--- v1/app/src/main.js
+++ v1/app/src/main.js
@@ -665,7 +665,7 @@ const menuItems = [
  * @func menuInitialize
  * @desc menuInitialize
  * @async
- * @param {void} 
+ * @param {void}
  * @return void
  * @throw error
  */
@@ -680,7 +680,7 @@ function menuInitialize() {
  * @func createShortCut
  * @desc デスクトップにショートカット作成、スタートメニューに登録
  * @async
- * @param {void} 
+ * @param {void}
  * @return void
  * @throw error
  */
@@ -688,7 +688,7 @@ function createShortCut() {
 	// windows用
 	if (isWin) {
 		let dist = path.join(userHome, 'Desktop', 'PLIS.lnk');		// 作成したいショートカットのパス (末尾の.lnkが必要)
-		let source = path.join(userHome, 'AppData', 'Local', 'HEMS_Logger', 'PLIS.exe'); // リンク元としたいディレクトリorファイルパス（本体）
+		let source = path.join(userHome, 'AppData', 'Local', 'PLIS', 'PLIS.exe'); // リンク元としたいディレクトリorファイルパス（本体）
 
 		// ショートカット作成コマンド
 		let command = `$WshShell = New-Object -ComObject WScript.Shell; $ShortCut = $WshShell.CreateShortcut("${dist}"); $ShortCut.TargetPath = "${source}"; $ShortCut.Save();`;
@@ -708,7 +708,7 @@ function createShortCut() {
  * @func saveConfig
  * @desc saveConfig
  * @async
- * @param {void} 
+ * @param {void}
  * @return void
  * @throw error
  */
@@ -734,7 +734,7 @@ async function saveConfig() {
  * @func savePersist
  * @desc savePersist
  * @async
- * @param {void} 
+ * @param {void}
  * @return void
  * @throw error
  */
