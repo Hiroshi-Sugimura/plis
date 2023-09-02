1fb8096c312caf46588b582ad3016f9bf6511d84
sugimrua@DAIV <hiroshi.sugimura@gmail.com>
2023-09-02 23:44:40 +0900

    パッケージわからん

--- v1/app/forge.config.js
+++ v1/app/forge.config.js
@@ -62,4 +62,17 @@ module.exports = {
       },
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'github-user-name',
+          name: 'github-repo-name'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
